55bb27ef3b143bd3e2f15e625be22387e01adb6f
Torbjørn Skårslette Stensrud <torbjornskarslettestensrud@tbs.local>
2024-09-22 11:42:01 +0200

    small bugfix on darkmode

--- frontend/src/layout/Navbar/Navbar.jsx
+++ frontend/src/layout/Navbar/Navbar.jsx
@@ -9,6 +9,7 @@ import useFetch from "../../hooks/useFetch";
 // Components
 import NavbarLink from './NavbarLink';
 import NavbarMenuTitle from "./NavbarMenuTitle";
+import DarkmodeContainer from "./DarkmodeContainer";
 
 function Navbar() {
     const { activeProject, setActiveProject, userUuid, setUserUuid, userName, setUserName, activeProjectName, setActiveProjectName, darkMode, setDarkMode } = useContext(GlobalContext);
@@ -172,28 +173,8 @@ function Navbar() {
                     <div onMouseEnter={handleShowDashboardMenu} className="font-semibold pl-2 pr-2 hover:cursor-default tracking-wide">
                         Dashboard
                     </div>
-
                     <div className="flex text-center items-center pl-3 h-full border-l dark:border-dark-default-border-color border-default-border-color">
-                        <div className="flex mr-3">
-                            <svg className="stroke-accent-color dark:stroke-dark-accent-color fill-none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" width="24" height="24" viewBox="0 0 24 24">
-                                <defs></defs>
-                                <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
-                            </svg>
-                        </div>
-                        <div className="relative w-10 h-5">
-                            <input onClick={toggleDarkModeContainer} type="checkbox" id="toggle" />
-                            <label htmlFor="toggle" className="slider"></label>
-                        </div>
-                        <div className="flex ml-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 26" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="stroke-accent-color dark:stroke-dark-accent-color fill-none">
-                                <circle cx="12" cy="12" r="5"></circle>
-                                <line x1="12" y1="1" x2="12" y2="3"></line>
-                                <line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line>
-                                <line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                            </svg>
-                        </div>
+                        <DarkmodeContainer />
                     </div>
                 </div>
             </div>
